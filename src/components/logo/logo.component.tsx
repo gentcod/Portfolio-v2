40c7c272b0d0a545dbcf2807f06083183c7e38a4
Oyefule <drelanorgent@gmail.com>
2024-02-25 16:47:30 +0100

    Modified Navigation and Web-Project component for responsiveness

--- src/components/logo/logo.component.tsx
+++ src/components/logo/logo.component.tsx
@@ -3,7 +3,7 @@ import { LogoContainer, LogoName, LogoDescription } from './logo.styles';
 const Logo = () => {
    return (
       <LogoContainer to={'/'}>
-            <LogoName>Oyefule Oluwatayo Oluwademilade</LogoName>
+            <LogoName>Oyefule Oluwatayo</LogoName>
             <LogoDescription>Web Developer, Graphics Designer</LogoDescription>
       </LogoContainer>
    )
